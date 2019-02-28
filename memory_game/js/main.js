b8e0c0f455bd9429b64943b04fa23fd81876253a
Samuel Pond Marritz <kim@Samuels-MacBook-Air.local>
2019-02-28 13:01:28 -0800

    adding game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,19 @@
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+cardsInPlay.push('king');
+cardsInPlay.push('queen');
+console.log('user flipped queen');
+console.log('user flipped king');
+
+if (cardsInPlay.length === 2) {   
+    if (cardOne === cardTwo) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again!");
+    }
+}
