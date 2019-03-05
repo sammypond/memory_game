f3e5c02d266928b04b699a70a40477568599a09e
Samuel Pond Marritz <kim@Samuels-MacBook-Air.local>
2019-03-04 21:24:21 -0800

    improved game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,7 +3,28 @@
 
 
 
-var cards = ['queen','queen','king','king'];
+var cards = [
+    {
+        rank: 'queen',
+        suit: 'hearts',
+        cardImage: 'images/queen-of-hearts.png'
+    }, 
+    {
+        rank: 'queen',
+        suit: 'diamonds',
+        cardImage: 'images/queen-of-diamonds.png'  
+    },
+    {
+        rank: 'king',
+        suit: 'hearts',
+        cardImage: 'images/king-of-hearts.png'
+    },
+    {
+        rank: 'king',
+        suit: 'diamonds',
+        cardImage: 'images/king-of-diamonds.png'
+    }
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -17,16 +38,13 @@ function checkForMatch() {
 };
 
 function flipCard(cardId){
-console.log("User flipped " + cards[cardId])
-cardsInPlay.push(cards[cardId]);
-/*var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("user flipped " + cardOne);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+    cardsInPlay.push(cards[cardId].rank);
+};
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("user flipped " + cardTwo);*/
-}
+
 
 
 
